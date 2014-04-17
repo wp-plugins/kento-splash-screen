86bb1aa5e604815fe072e499699776fe2020921b
kentothemes <kentothemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-17 06:02:42 +0000

    update 1.3
    git-svn-id: https://plugins.svn.wordpress.org/kento-splash-screen/tags/1.3@895665 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kento-splash-screen-admin.php
+++ kento-splash-screen-admin.php
@@ -1,24 +1,66 @@
 <?php
-		if($_POST['oscimp_hidden'] == 'Y') {
-			//Form data sent
-			
 
+	if(empty($_POST['kento_splash_screen_hidden']))
+		{
+			$kento_splash_screen_bg_color = get_option( 'kento_splash_screen_bg_color' );
+			$kento_splash_screen_bg_img = get_option( 'kento_splash_screen_bg_img' );			
+			$kento_splash_screen_border_size = get_option( 'kento_splash_screen_border_size' );
+			$kento_splash_screen_border_color = get_option( 'kento_splash_screen_border_color' );
+			$kento_splash_screen_content = get_option( 'kento_splash_screen_content' );			
+			$kento_splash_screen_demo = get_option( 'kento_splash_screen_demo' );
+			$kento_splash_screen_width = get_option( 'kento_splash_screen_width' );			
+			$kento_splash_screen_height = get_option( 'kento_splash_screen_height' );
+			$kento_splash_screen_left = get_option( 'kento_splash_screen_left' );					
+		}
+	else{
+		if($_POST['kento_splash_screen_hidden'] == 'Y') {
+		
+		
+		
+		
+			if(empty($_POST['kento_splash_screen_demo']))
+				{
+				$kento_splash_screen_demo ="";
+				}
+			else
+				{
+					$kento_splash_screen_demo = $_POST['kento_splash_screen_demo'];
+				}
+			update_option('kento_splash_screen_demo', $kento_splash_screen_demo);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 			$kento_splash_screen_bg_color = $_POST['kento_splash_screen_bg_color'];
 			update_option('kento_splash_screen_bg_color', $kento_splash_screen_bg_color);
 
 			$kento_splash_screen_bg_img = $_POST['kento_splash_screen_bg_img'];
 			update_option('kento_splash_screen_bg_img', $kento_splash_screen_bg_img);
 
-			$kento_splash_screen_border_size = sanitize_text_field($_POST['kento_splash_screen_border_size']);
+			$kento_splash_screen_border_size = $_POST['kento_splash_screen_border_size'];
 			update_option('kento_splash_screen_border_size', $kento_splash_screen_border_size);
 
 			$kento_splash_screen_border_color = $_POST['kento_splash_screen_border_color'];
 			update_option('kento_splash_screen_border_color', $kento_splash_screen_border_color);
 			
 			$kento_splash_screen_content = $_POST['kento_splash_screen_content'];
-			update_option('kento_splash_screen_content', $kento_splash_screen_content);			
+			update_option('kento_splash_screen_content', $kento_splash_screen_content);	
+
+			$kento_splash_screen_width = $_POST['kento_splash_screen_width'];
+			update_option('kento_splash_screen_width', $kento_splash_screen_width);				
 			
+			$kento_splash_screen_height = $_POST['kento_splash_screen_height'];
+			update_option('kento_splash_screen_height', $kento_splash_screen_height);
 			
+			$kento_splash_screen_left = $_POST['kento_splash_screen_left'];
+			update_option('kento_splash_screen_left', $kento_splash_screen_left);
 			?>
 			<div class="updated"><p><strong><?php _e('Changes Saved.' ); ?></strong></p>
             </div>
@@ -27,24 +69,15 @@
             
             
 			<?php
-		} else {
-			//Normal page display
-			$kento_splash_screen_bg_color = get_option( 'kento_splash_screen_bg_color' );
-			$kento_splash_screen_bg_img = get_option( 'kento_splash_screen_bg_img' );			
-			$kento_splash_screen_border_size = get_option( 'kento_splash_screen_border_size' );
-			$kento_splash_screen_border_color = get_option( 'kento_splash_screen_border_color' );
-			$kento_splash_screen_content = get_option( 'kento_splash_screen_content' );			
-			
-			
-		}
-
+		} 
+	}
 ?>
 
 
 <div class="wrap skill-bars">
 	<div id="icon-tools" class="icon32"><br></div><?php echo "<h2>".__('Kento Splash Screen Settings')."</h2>";?>
 		<form  method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
-	<input type="hidden" name="oscimp_hidden" value="Y">
+	<input type="hidden" name="kento_splash_screen_hidden" value="Y">
         <?php settings_fields( 'kento_splash_screen_plugin_options' );
 				do_settings_sections( 'kento_splash_screen_plugin_options' );
 			
@@ -52,19 +85,80 @@
 
 <table class="form-table">
                
+<?php
+	if(empty($_POST['kento_splash_screen_hidden']))
+		{}
+else{
+?>
 
-   
+	<tr valign="top">
+		<th scope="row">Use this Shortcode:
+		</th>
+		<td style="vertical-align:middle;">
+			<input  type="text" name="kento_splash_screen_shortcode" onClick="this.select();" size="auto" id="kento-splash-screen-shortcode"  value ="[kento-splash-screen]"  > ** Use this shortcode to display splash screen.
 
+		</td>
+	</tr>   
+
+
+<?php } ?>
+
+
+
+
+	<tr valign="top">
+		<th scope="row">Test Mode:
+		</th>
+		<td style="vertical-align:middle;">
+			<input type="checkbox" name="kento_splash_screen_demo" size="7" id="kento-splash-screen-demo"  value ="1" <?php if ( $kento_splash_screen_demo == 1 ) echo "checked"; ?> >** Will popup for every refresh.
+
+            
+		</td>
+	</tr>   
+
+
+<tr valign="top">
+		<th scope="row">Box Width:
+		</th>
+		<td style="vertical-align:middle;">
+			<input type="text" name="kento_splash_screen_width" size="7" id="kento-splash-screen-width"  value ="<?php if ( isset( $kento_splash_screen_width ) && !empty($kento_splash_screen_width) ) { echo $kento_splash_screen_width; } else { echo "500"; } ?>"> px
+
+            
+		</td>
+	</tr>
+
+<tr valign="top">
+		<th scope="row">Box Height:
+		</th>
+		<td style="vertical-align:middle;">
+			<input type="text" name="kento_splash_screen_height" size="7" id="kento-splash-screen-height"  value ="<?php if ( isset( $kento_splash_screen_height ) && !empty($kento_splash_screen_height) ) { echo $kento_splash_screen_height; } else { echo "300"; } ?>"> px
+
+            
+		</td>
+	</tr>
+
+<tr valign="top">
+		<th scope="row">Position Left(%):
+		</th>
+		<td style="vertical-align:middle;">
+			<input type="text" name="kento_splash_screen_left" size="7" id="kento-splash-screen-left"  value ="<?php if ( isset( $kento_splash_screen_left ) && !empty($kento_splash_screen_left) ) { echo $kento_splash_screen_left; } else { echo "30"; } ?>">% (**If you increase/decrease box width then you must adjust position left value.)
+
+            
+		</td>
+	</tr>
 
 	<tr valign="top">
 		<th scope="row">Background Color:
 		</th>
 		<td style="vertical-align:middle;">
-			<input type="text" name="kento_splash_screen_bg_color" size="7" id="kento-splash-screen-bg-color"  value ="<?php if ( isset( $kento_splash_screen_bg_color ) ) echo $kento_splash_screen_bg_color; ?>">
+			<input type="text" name="kento_splash_screen_bg_color" size="7" id="kento-splash-screen-bg-color"  value ="<?php if ( isset( $kento_splash_screen_bg_color ) ) echo $kento_splash_screen_bg_color; ?>"> 
 
             
 		</td>
 	</tr>
+    
+    
+    
     
 	<tr valign="top">
 		<th scope="row">Background Image URL:
@@ -72,13 +166,16 @@
 		<td style="vertical-align:middle;">
 		<input type="text" name="kento_splash_screen_bg_img" size="50"  id="kento-splash-screen-bg-img" value="<?php if ( isset( $kento_splash_screen_bg_img ) ) echo $kento_splash_screen_bg_img; ?>"  /><br />
             <div id="kento-splash-screen-bg-img-preview">
+            <?php
+            if(!empty($kento_splash_screen_bg_img))
+				{
+			?>
             <img src="" />
+            <?php
+            }
+			?>
             </div>
-<script>
-var kento_splash_screen_bg_img = jQuery('#kento-splash-screen-bg-img').attr("value");
-jQuery('#kento-splash-screen-bg-img-preview img').attr("src",kento_splash_screen_bg_img);
-
-</script>      
+     
   
 		</td>
 	</tr>    
@@ -124,7 +221,14 @@ jQuery('#kento-splash-screen-bg-img-preview img').attr("src",kento_splash_screen
 		</form>
 
 
-        
+<script>
+jQuery(document).ready(function(jQuery)
+	{	
+	var kento_splash_screen_bg_img = jQuery('#kento-splash-screen-bg-img').val();
+	jQuery('#kento-splash-screen-bg-img-preview img').attr("src",kento_splash_screen_bg_img);
+	jQuery('#kento-splash-screen-bg-color, #kento-splash-screen-border-color').wpColorPicker();
+	});
+</script> 
         
         
         
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: splash screen, splash page, welcome page, landing page, splashscreen, splash, popup box, Popup Dialog, Popup, popups
 Requires at least: 3.7
 Tested up to: 3.8
-Stable tag: 1.2
+Stable tag: 1.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -12,12 +12,17 @@ Splash scree box for fisrt time visit or new visitors
 
 == Description ==
 
-Display message or HTML content via splash popup box 
+Display message or HTML content via splash popup box.
 
-Live Preview: http://kentothemes.com/demo/kento-splash-screen/kento-splash-screen/
+Plugin Details and Support
+http://kentothemes.com/items/plugins/kento-splash-screen/
 
 Plugin Features
 
+* Test Mode.
+* iframe Support.
+* Custom Width of popup box.
+* Custom Height of popup box.
 * Unlimited Background Color.
 * Unlimited Background Image.
 * Custom Border Size.
@@ -31,8 +36,8 @@ Plugin Features
 
 1. Install as regular WordPress plugin.
 2. Go your Pluings setting via WordPress Dashboard and activate it.
-3. After actvating you will see a menu on left side and go to "Kento Splash Screen" setting page and change your setting and save it.
-4. Paste this shortcode [kento-splash-screen] in which page you want to dissplay popup box.
+3. After actvating you will see a menu on left side and go to "<strong>Kento Splash Screen</strong>" setting page and change your setting and save it.
+4. Paste this shortcode <strong>`[kento-splash-screen]`</strong> in which page you want to dissplay popup box.
 5. Enjoy!!!
 
 
@@ -40,11 +45,24 @@ Plugin Features
 
 1. Demo popup box.
 2. Admin settings.
-
+3. Yotube Video on popup.
 
 
 == Changelog ==
 
+= 1.3 =
+* Added Width.
+* Added Height.
+* Added Left position.
+
+= 1.2 =
+* Display Shortcode after save settings.
+* javascript error fix.
+
+= 1.1 =
+* Added Test Mode.
+* javascript problem fix.
+
 = 1.0 =
-* Initial release
+* Initial release.
 
